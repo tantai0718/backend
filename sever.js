c52e3cc66771d2790a06b845bbf107bf1e8bd2f0
Tấn <tantai0718@gmail.com>
2026-01-17 14:49:44 +0700

    demo3

--- sever.js
+++ sever.js
@@ -1,11 +1,14 @@
-const { createServer } = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/a', (req, res) => {
+    res.send('check')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
